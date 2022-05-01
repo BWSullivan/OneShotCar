c014289e630abd7d6d99d0a898a1f9a8d2cb32e3
Roshin Lal <66693974+Roshinl@users.noreply.github.com>
2022-04-30 22:57:12 -0500

    Update script.js

--- static/fonts/script.js
+++ static/fonts/script.js
@@ -1,8 +1,77 @@
+/*QUICK JS SWITCHER
+    Website does not run correctly on newly opened windows or incognito mode most likely due to cache memory or chrome settings. It only works when I click on the button that uses the JS file.
+    Quick  JS Switcher refreshes the page so that this is not an issue.
+*/
+if (chromeContentSettings) {
+
+  var extractHostname = new RegExp('^(?:f|ht)tp(?:s)?\://([^/]+)', 'im'),
+    forbiddenOrigin = /(chrome\:\/\/|chrome-extension\:\/\/)/g,
+    incognito,
+    url,
+    setting,
+    tabId,
+    matchForbiddenOrigin;
+
+  chrome.tabs.onUpdated.addListener(function (tabId, props, tab) {
+    // Prevent multiple calls
+    if (props.status == "loading" && tab.selected) {
+      //console.info("onUpdated");
+      getSettings();
+    }
+  });
+
+  chrome.tabs.onHighlighted.addListener(function () {
+    //console.info("onHighlighted");
+    getSettings();
+  });
+
+  chrome.windows.onFocusChanged.addListener(function () {
+    //console.info("onFocusChanged");
+    getSettings();
+  });
+
+  chrome.windows.getCurrent(function () {
+    getSettings();
+  });
+
+  chrome.browserAction.onClicked.addListener(changeSettings);
+
+
+  chrome.commands.onCommand.addListener(function (command) {
+    if (command == "toggle-qjs") {
+
+      changeSettings();
+
+    }
+  });
+
+  chrome.storage.onChanged.addListener(function (callback) {
+
+    for (var index in callback) {
+      if (callback.hasOwnProperty(index)) {
+        console.log(callback[index]);
+        if (index === 'version') {
+          cache[index] = callback[index].newValue;
+        } else {
+            cache[index] = JSON.parse(callback[index].newValue);
+        }
+      }
+    }
+
+    refresh();
+
+  });
+
+
+} else {
+  chrome.browserAction.onClicked.addListener(openJsPanel.call());
+}
+
+/*CSS TOGGLE THEME*/
 
 function toggleTheme() {
   // Obtains an array of all <link> elements.
   var theme = document.getElementsByTagName('link')[0];
-//  console.log("got to toggleTheme function")
 
   // Change the value of href attribute to change the css sheet.
   if (theme.getAttribute('href') == '/static/dist/output.css') {
@@ -15,7 +84,6 @@ function toggleTheme() {
 }
 
 function setDefault(){
-//  console.log("got to setDefault function")
   theme.setAttribute('href', '/static/dist/output.css');// dist
 }
 
@@ -31,5 +99,4 @@ function keepCSS(){
 window.onload = function(){
   setDefault();
   keepCSS();
-//  console.log("got to onload function")
 }
